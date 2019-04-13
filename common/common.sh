ee509ef2728b7e103bcbc3707b40ff3be6d1adfb
tree <33415872+Treehuang@users.noreply.github.com>
2019-04-13 20:16:01 +0800

    修改缩进

--- common/common.sh
+++ common/common.sh
@@ -14,52 +14,52 @@ echo -e "\033[?25l"
 log=${COMMON_DIR}/../log/info.log
 
 function call_function {
-	func=$1
-	info=$2
+    func=$1
+    info=$2
 	
-	# 检测是否有tmp.txt(记录每个函数最后的执行状态，succ, fail)，先消除干净
-	if [[ -f ${COMMON_DIR}/tmp.txt ]]; then
-		rm -rf ${COMMON_DIR}/tmp.txt
+    # 检测是否有tmp.txt(记录每个函数最后的执行状态，succ, fail)，先消除干净
+    if [[ -f ${COMMON_DIR}/tmp.txt ]]; then
+	rm -rf ${COMMON_DIR}/tmp.txt
+    fi
+	
+    # 函数运行在子进程中
+    {
+	${func} > ${log} 2>&1
+	if [[ $? -eq 0 ]]; then
+	    echo 'succ' > ${COMMON_DIR}/tmp.txt
+	else
+	    echo 'fail' > ${COMMON_DIR}/tmp.txt
 	fi
+    }&
 	
-	# 函数运行在子进程中
-	{
-		${func} > ${log} 2>&1
-		if [[ $? -eq 0 ]]; then
-			echo 'succ' > ${COMMON_DIR}/tmp.txt
-		else
-			echo 'fail' > ${COMMON_DIR}/tmp.txt
-		fi
-	}&
-	
-	status=true
-	# 主进程检测函数的运行状态(检测tmp.txt)
-	while [[ ${status} = 'true' ]];
+    status=true
+    # 主进程检测函数的运行状态(检测tmp.txt)
+    while [[ ${status} = 'true' ]];
+    do
+        # 光标旋转效果(等同于loading)
+	for j in '-' '\' '|' '/'
 	do
-		# 光标旋转效果(等同于loading)
-		for j in '-' '\' '|' '/'
-		do
-			printf "\e[33m====> $info %s\r\e[0m" ${j}
-			sleep 0.1
-		done
-		
-		# 检测函数执行状态
-		if [[ -f ${COMMON_DIR}/tmp.txt ]]; then
-			tmp=$( cat ${COMMON_DIR}/tmp.txt )
-			
-			if [[ ${tmp} = 'succ' ]]; then
-				status=false
-				printf "\e[33m%-38s" "${info}"
-				ansi --green "[DONE]  [OK]"
-				rm -rf ${COMMON_DIR}/tmp.txt
-			else
-				status=false
-				printf "\e[33m%-38s" "${info}"
-				ansi --red "[DONE]  [FAIL]"
-				echo -n -e "\033[?25h"        # 显示光标
-				rm -rf ${COMMON_DIR}/tmp.txt
-				exit
-			fi
-		fi
+	    printf "\e[33m====> $info %s\r\e[0m" ${j}
+	    sleep 0.1
 	done
+		
+	# 检测函数执行状态
+	if [[ -f ${COMMON_DIR}/tmp.txt ]]; then
+	    tmp=$( cat ${COMMON_DIR}/tmp.txt )
+			
+	    if [[ ${tmp} = 'succ' ]]; then
+	        status=false
+		printf "\e[33m%-38s" "${info}"
+		ansi --green "[DONE]  [OK]"
+		rm -rf ${COMMON_DIR}/tmp.txt
+	    else
+		status=false
+		printf "\e[33m%-38s" "${info}"
+		ansi --red "[DONE]  [FAIL]"
+		echo -n -e "\033[?25h"        # 显示光标
+		rm -rf ${COMMON_DIR}/tmp.txt
+		exit
+	    fi
+	fi
+    done
 }
