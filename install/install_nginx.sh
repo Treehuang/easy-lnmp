1a467a95ee9040bdd78f4d558b2733254630a563
tree <33415872+Treehuang@users.noreply.github.com>
2019-04-13 20:12:18 +0800

    修改缩进

--- install/install_nginx.sh
+++ install/install_nginx.sh
@@ -4,31 +4,31 @@ CURRENT_DIR=$( cd "$(dirname ${BASH_SOURCE})"; pwd -P )
 TPL_DIR=${CURRENT_DIR}/../tpl
 
 function install_nginx {
-	apt-get install -y nginx
+    apt-get install -y nginx
 	
-	# 创建站点目录
-	rm -rf /var/www/html
-	rm -rf /var/www/80 /var/www/8080
+    # 创建站点目录
+    rm -rf /var/www/html
+    rm -rf /var/www/80 /var/www/8080
 	
-	if [[ ! -d /var/www ]]; then
-	    mkdir /var/www
-	fi    
+    if [[ ! -d /var/www ]]; then
+        mkdir /var/www
+    fi    
 
-	mkdir /var/www/80 /var/www/8080
-	touch /var/www/80/index.php /var/www/8080/index.php
+    mkdir /var/www/80 /var/www/8080
+    touch /var/www/80/index.php /var/www/8080/index.php
 
-	echo '<?php phpinfo();' > /var/www/80/index.php
-	echo '<?php phpinfo();' > /var/www/8080/index.php
+    echo '<?php phpinfo();' > /var/www/80/index.php
+    echo '<?php phpinfo();' > /var/www/8080/index.php
 
-	# 配置站点以及兼容php
-	if [[ ! -f '8080.conf' ]]; then
-		touch /etc/nginx/conf.d/8080.conf
-	fi
+    # 配置站点以及兼容php
+    if [[ ! -f '8080.conf' ]]; then
+        touch /etc/nginx/conf.d/8080.conf
+    fi
 
-	cat ${TPL_DIR}/nginx_site_conf.tpl > /etc/nginx/conf.d/8080.conf
-	cat ${TPL_DIR}/default_nginx_site_conf.tpl > /etc/nginx/sites-available/default
+    cat ${TPL_DIR}/nginx_site_conf.tpl > /etc/nginx/conf.d/8080.conf
+    cat ${TPL_DIR}/default_nginx_site_conf.tpl > /etc/nginx/sites-available/default
 	
-	# 启动nginx
-	service nginx stop
-	service nginx start
+    # 启动nginx
+    service nginx stop
+    service nginx start
 }
