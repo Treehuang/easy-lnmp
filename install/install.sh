f777e320708fe7113d6a89d4ce8e8a5675ed9575
tree <33415872+Treehuang@users.noreply.github.com>
2019-04-13 20:47:06 +0800

    修改间距

--- install/install.sh
+++ install/install.sh
@@ -57,8 +57,7 @@ function init_repositories {
     # add-apt-repository -y ppa:nginx/stable
     # grep -rl ppa.launchpad.net /etc/apt/sources.list.d/ | xargs sed -i 's/ppa.launchpad.net/launchpad.proxy.ustclug.org/g'
     apt install curl gnupg2 ca-certificates lsb-release
-    echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
+    echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
     
     curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
     apt-key fingerprint ABF5BD827BD9BF62
